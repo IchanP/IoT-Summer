7c873d6af87128a6d26bbcf5860834372d7ddce9
Pontus Grandin <smeaurix@gmail.com>
2024-06-10 11:21:30 +0200

    initial commit

---
+++ boot.py
@@ -0,0 +1,23 @@
+import network, utime
+from machine import Pin
+from config import WIFI_SSID, WIFI_PASSWORD
+
+pin = Pin("LED", Pin.OUT)
+
+def connect():
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(WIFI_SSID, WIFI_PASSWORD)
+        while not sta_if.isconnected():
+            print("Attempting to connect to network...")
+            utime.sleep(1)
+    if sta_if.isconnected():
+        print("Connected to network")
+        print(sta_if.ifconfig())
+        pin.toggle()
+
+
+print("Connecting to wifi")
+connect()
---
+++ scripts.py
@@ -0,0 +1,30 @@
+from time import sleep
+import machine
+import json
+
+led = machine.Pin("LED", machine.Pin.OUT)
+
+def get_sensor_data(sensor):
+    sensor.measure()
+    temp = sensor.temperature()
+    hum = sensor.humidity()
+    return temp, hum
+
+def handle_subscription(topic, msg):
+    print(topic)
+    # TODO add a condition to check if the topic is the same as the one we are subscribing to
+    if msg == b'ON':
+        led.value(1)
+    elif msg == b'OFF':
+        led.value(0)
+
+def json_preparation(key, value):
+    data = {
+        key: value
+    }
+    return json.dumps(data)
+
+def reset():
+    print('Resetting the device...') 
+    sleep(5)
+    machine.reset()

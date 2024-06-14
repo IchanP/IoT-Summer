f27bdef23f6977d2c38f39df63bb55a2aa3019df
Pontus Grandin <smeaurix@gmail.com>
2024-06-14 12:28:56 +0200

    turn led on or off based on moisture level

--- InputOutput/InputOutputFacade.py
+++ InputOutput/InputOutputFacade.py
@@ -7,10 +7,17 @@ class InputOutputFacade:
     def __init__(self):
         self.moistureSensor = MoistureHandler(26)
        # self.dhtSensor = DHTHandler()
-       # self.led = LEDHandler()
+        self.led = LEDHandler(16)
 
     def read_all_sensors(self):
         moistureReading = self.moistureSensor.read_percentage_moisture()
+        self._enable_led(moistureReading)
         # temp, hum = self.dhtSensor.read()
         # TODO - convert to JSON?
         return moistureReading
+    
+    def _enable_led(self, moistureReading):
+        if moistureReading < 30:
+            self.led.turn_on()
+        elif self.led.is_on() and moistureReading > 30:
+            self.led.turn_off()
--- InputOutput/LEDHandler.py
+++ InputOutput/LEDHandler.py
@@ -4,3 +4,13 @@ from machine import Pin
 class LEDHandler:
     def __init__(self, pin):
         self.led = machine.Pin(pin, Pin.OUT)
+
+    # TODO change this to check the current time and if it's in operable hours and turned on return true.    
+    def is_on(self):
+        return self.led.value() == 1
+
+    def turn_on(self):
+        self.led.value(1)
+    
+    def turn_off(self):
+        self.led.value(0)
